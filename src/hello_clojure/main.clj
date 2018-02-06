52c8fe34d8e3ecd14b407c800146ea0bf73d15e8
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2018-02-06 11:48:40 +0800

    :pencil: 增加了初学clojure的相关书籍

---
+++ src/hello_clojure/main.clj
@@ -0,0 +1,10 @@
+(ns hello-clojure.main
+  (:gen-class))
+
+1
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/hello_clojure/main_test.clj
@@ -0,0 +1,7 @@
+(ns hello-clojure.main-test
+  (:require [clojure.test :refer :all]
+            [hello-clojure.main :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
