00557595ed39aec0a6f97c895dda6873f496b5e1
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2018-02-06 11:57:26 +0800

    :pencil: do exercise with clojure

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  ;; 主函数入口
   :main ^:skip-aot hello-clojure.main
+
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
