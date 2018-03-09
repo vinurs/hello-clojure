9b0fc4c37fe9d103421afc48dfb53f566bb3e829
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2018-03-09 12:52:38 +0800

    增加了高级内容

---
+++ src/hello_clojure/funcs/some.clj
@@ -0,0 +1,37 @@
+(ns hello-clojure.funcs.some)
+
+
+;; (some pred coll)
+;; 将pred作为函数作用于coll里面的每一个元素，如果遇到逻辑真，那就返回那个值，
+;; 否则返回nil
+
+
+;; 2 is even, so `some` stops there, 3 and 4 are never tested
+(some even? '(1 2 3 4))
+;;=> true
+
+;; they are all odd, so not true, i.e. nil
+(some even? '(1 3 5 7))
+;;=> nil
+
+;; 直接返回1
+(some + '(1 2 3 4 5))
+
+
+(some true? [false false false])
+;;=> nil
+
+(some true? [false true false])
+;;=> true
+
+(some true? [true true true])
+;;=> true
+
+
+(some #{"init"} '("init" 1 2 3))
+
+
+;; the hash (as function) returns a nil for the key of '3';
+;; nil在逻辑里面表示假，所以继续寻找key为2的值，就是"two"
+(some {2 "two" 3 nil} [nil 3 2])
+;;=> "two"
